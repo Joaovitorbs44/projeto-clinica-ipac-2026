63bfb539d222353f65b15e92ca1d6ab9ee06511a
Joao Vitor <Joaovitorbsouza49@gmail.com>
2026-09-06 14:00:07 -0300

    Update index.php

--- site-ipac/api/index.php
+++ site-ipac/api/index.php
@@ -1,10 +1,3 @@
 <?php
 
-
-header('Content-Type: text/plain; charset=utf-8');
-
-echo "PHP NA VERCEL FUNCIONANDO";
-
-declare(strict_types=1);
-
-require_once __DIR__ . '/../src/pages/ipac.php';
+echo 'PHP funcionando na Vercel';
